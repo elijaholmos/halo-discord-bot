241514825eaaeec9eb1e1309e1091140bd9bcd33
elijaholmos <eli.olmos9@gmail.com>
2022-01-04 10:33:44 -0700

    change rollup plugins

--- extension/rollup.config.js
+++ extension/rollup.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from 'rollup';
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import { emptyDir } from 'rollup-plugin-empty-dir';
+import { terser } from 'rollup-plugin-terser';
+import zip from "rollup-plugin-zip";
+
+const isProduction = !process.env.ROLLUP_WATCH;
 
 export default defineConfig({
-    input: 'src/manifest.json',
-    output: {
-        dir: 'dist',
-        format: 'esm',
-    },
-    plugins: [
-        chromeExtension(),
-        simpleReloader(),
-        resolve(),
-        commonjs(),
-        emptyDir(),
-    ],
+	input: 'src/manifest.json',
+	output: {
+		dir: 'dist',
+		format: 'esm',
+	},
+	plugins: [
+		chromeExtension(),
+		simpleReloader(),
+		resolve(),
+		emptyDir(),
+		isProduction && terser(),
+		isProduction && zip({ dir: 'dist_packed' }),
+	],
 });

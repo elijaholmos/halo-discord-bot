170c6a7348d34d9519f014a3dfd22c5fe17729f4
elijaholmos <eli.olmos9@gmail.com>
2022-09-01 16:47:39 -0700

    feat(CookieWatcher): increase refresh interval to 6hrs

--- classes/CookieWatcher.js
+++ classes/CookieWatcher.js
@@ -20,7 +20,7 @@ import path from 'node:path';
 import { Firebase, Halo } from '.';
 // Watch for Cookie updates in Firebase and manually refresh Halo tokens when necessary
 export class CookieWatcher {
-    static REFRESH_INTERVAL = 1000 * 60 * 60 * 3; //3 hours
+    static REFRESH_INTERVAL = 1000 * 60 * 60 * 6; //6 hours
     static timeouts = new Map();    //to track and clear timeouts
 
     static async init() {
